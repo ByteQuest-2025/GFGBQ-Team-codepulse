8480681b8eda42d0521d5c7450f8f063c5f18efb
Auxilus08 <tiwariakshat028@gmail.com>
2026-01-03 15:56:06 +0530

    chore: add auth emulator support

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { connectAuthEmulator, getAuth } from 'firebase/auth'
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 import { firebaseConfig } from './config'
 
@@ -8,6 +8,11 @@ const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 if (import.meta.env.DEV) {
   auth.settings.appVerificationDisabledForTesting = true
+}
+
+if (import.meta.env.VITE_USE_FIREBASE_EMULATOR === 'true') {
+  const emulatorUrl = import.meta.env.VITE_AUTH_EMULATOR_URL || 'http://127.0.0.1:9099'
+  connectAuthEmulator(auth, emulatorUrl, { disableWarnings: true })
 }
 
 const db = getFirestore(app)
